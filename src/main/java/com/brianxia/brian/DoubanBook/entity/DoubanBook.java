1c9cd2d59a50318724013f2bf59496f8782d6cbf
BrianXia <139652626@qq.com>
2018-02-13 23:57:30 +0800

    增加lombok

--- src/main/java/com/brianxia/brian/DoubanBook/entity/DoubanBook.java
+++ src/main/java/com/brianxia/brian/DoubanBook/entity/DoubanBook.java
@@ -1,5 +1,8 @@
 package com.brianxia.brian.DoubanBook.entity;
 
+import lombok.Data;
+
+@Data
 public class DoubanBook {
 
     private String name;
@@ -7,44 +10,4 @@ public class DoubanBook {
     private String publisher;
     private Float star;
     private Float price;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Float getStar() {
-        return star;
-    }
-
-    public void setStar(Float star) {
-        this.star = star;
-    }
-
-    public Float getPrice() {
-        return price;
-    }
-
-    public void setPrice(Float price) {
-        this.price = price;
-    }
 }
